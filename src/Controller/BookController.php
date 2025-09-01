d5745c87984f99ceb7eff217e0daa873542458ba
cmouns <mounir.sebti33@gmail.com>
2025-09-01 18:36:46 +0200

    add favorites book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,11 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+ 
 
-#[Route('/editor/book')]
+
+
 final class BookController extends AbstractController
 {
-    #[Route(name: 'app_book_index', methods: ['GET'])]
+    #[Route('/editor/book', name: 'app_book_index', methods: ['GET'])]
     public function index(BookRepository $bookRepository): Response
     {
         return $this->render('book/index.html.twig', [
@@ -22,7 +25,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_book_new', methods: ['GET', 'POST'])]
+    #[Route('/editor/book/new', name: 'app_book_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $book = new Book();
@@ -42,7 +45,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_show', methods: ['GET'])]
+    #[Route('/editor/book/show/{id}', name: 'app_book_show', methods: ['GET'])]
     public function show(Book $book): Response
     {
         return $this->render('book/show.html.twig', [
@@ -50,7 +53,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_book_edit', methods: ['GET', 'POST'])]
+    #[Route('/editor/book/{id}/edit', name: 'app_book_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(BookType::class, $book);
@@ -68,7 +71,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_delete', methods: ['POST'])]
+    #[Route('/editor/book/{id}/delete', name: 'app_book_delete', methods: ['POST'])]
     public function delete(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->getPayload()->getString('_token'))) {
@@ -78,4 +81,50 @@ final class BookController extends AbstractController
 
         return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/book/{id}/favorite', name: 'app_add_book_favorite')]
+    public function toggleFavorite(?Book $book, EntityManagerInterface $em): Response
+    {
+        if (!$book) {
+            throw $this->createNotFoundException('Livre non trouvé');
+        }
+
+        $user = $this->getUser();
+
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($user->getFavorites()->contains($book)) {
+            $user->removeFavorite($book);
+        } else {
+            $user->addFavorite($book);
+        }
+
+        $em->flush();
+
+        return $this->redirectToRoute('app_show_book_favorite');
+    }
+
+     #[Route('/show/favorites', name: 'app_show_book_favorite')]
+    public function showFavorite(EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $favorites = $user->getFavorites();
+
+        if ($favorites->count() === 0) {
+            $this->addFlash('info', 'Veuillez ajouter des favoris');
+        }
+
+        return $this->render('book/favorites.html.twig', [
+            'favorites' => $favorites,
+        ]);
+    }
+
+
 }
